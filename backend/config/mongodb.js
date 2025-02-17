bb4a584c110127891848976c85430811ac12cce9
Muhammad Hanzla <mirzamhanzla@gmail.com>
2025-02-18 00:27:17 +0500

    Remove Duplication

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 
-const mongoUri = 'mongodb+srv://alisaif2617:Ali11297@fyp.you7dai.mongodb.net/influencerHarbor?retryWrites=true&w=majority&appName=fyp';
-// const mongoUri = 'mongodb://localhost:27017/fyp';
-// const mongoUri = 'mongodb+srv://sabirrizwan321:vrKhbsCvg1Sj4gBI@cluster0.q3akg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 
 mongoose.connect(mongoUri)
   .then(() => console.log('Connected to MongoDB Atlas'))
